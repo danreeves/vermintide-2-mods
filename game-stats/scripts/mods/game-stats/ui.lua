cb1522174709349474fe9a9c303b2d45911ba957
Dan Reeves <heydanreeves@gmail.com>
2019-01-20 00:46:54 +0000

    i got some dropdowns woop

--- game-stats/scripts/mods/game-stats/ui.lua
+++ game-stats/scripts/mods/game-stats/ui.lua
@@ -1,6 +1,24 @@
 local mod = get_mod('game-stats')
 local simple_ui = get_mod("SimpleUI")
 
+local classes = mod.classes
+local difficulties = mod.difficulties
+local levels = mod.levels
+
+function to_localised_options(list, SettingsTable)
+    local options = {}
+    for i, key in pairs(list) do
+        local settings = SettingsTable[key]
+        local display_name = settings.display_name
+        options[Localize(display_name)] = i
+    end
+    return options
+end
+
+local class_options = to_localised_options(classes, CareerSettings)
+local difficulty_options = to_localised_options(difficulties, DifficultySettings)
+local level_options = to_localised_options(levels, LevelSettings)
+
 mod.create_window = function(self)
 	local native_screen_width, native_screen_height = Application.resolution()
 	local width = 800
@@ -9,9 +27,13 @@ mod.create_window = function(self)
 	local y = (1080 / 2) - (height / 2)
 	local window = simple_ui:create_window('mission stats', {x, y}, {width, height})
 	window:create_title('title', 'Mission Stats')
-	window:create_close_button('close')
-	window:init()
+    window:create_close_button('close')
 
+    window:create_dropdown('classes', {20, 25}, {200, 30}, "top_left", class_options)
+    window:create_dropdown('difficulties', {240, 25}, {200, 30}, "top_left", difficulty_options)
+    window:create_dropdown('levels', {460, 25}, {200, 30}, "top_left", level_options)
+
+	window:init()
 	window:bring_to_front()
 
 	window._was_closed = false
