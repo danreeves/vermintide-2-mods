bd5c3627f187056ff21ec3d55a5020016ec762e9
Dan Reeves <hey@danreev.es>
2019-03-03 19:28:21 +0000

    suicide mod

---
+++ suicide/scripts/mods/suicide/suicide.lua
@@ -0,0 +1,21 @@
+local mod = get_mod("suicide")
+
+-- Are we currently loaded at the inn?
+-- from Zaphio
+local function is_at_inn()
+  local game_mode = Managers.state.game_mode
+  if not game_mode then return nil end
+  return game_mode:game_mode_key() == "inn"
+end
+
+mod:command("die", "Kill yourself", function()
+  if is_at_inn() then
+    mod:echo("You cannot die in the keep")
+    return
+  end
+
+  Managers.chat:send_chat_message(1, 1, "Guess I'll die-die...")
+  local player_unit = Managers.player:local_player().player_unit
+  local death_system = Managers.state.entity:system("death_system")
+  death_system:kill_unit(player_unit, {})
+end)
---
+++ suicide/scripts/mods/suicide/suicide_data.lua
@@ -0,0 +1,7 @@
+local mod = get_mod("suicide")
+
+return {
+  name = "Suicide",
+  description = mod:localize("mod_description"),
+  is_togglable = true,
+}
