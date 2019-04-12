3208fb0ac2772e0cde0faf60a5b07b1853cef526
Dan Reeves <hey@danreev.es>
2019-04-12 02:08:00 +0100

    [custom frames] why arent you working

--- custom-frames/scripts/mods/custom-frames/custom-frames.lua
+++ custom-frames/scripts/mods/custom-frames/custom-frames.lua
@@ -1,3 +1,15 @@
 local mod = get_mod("custom-frames")
 
--- UIPlayerPortraitFrameSettings.frame_holly_05[1].texture = ":sad_robin:"
+UIPlayerPortraitFrameSettings.frame_holly_04[1].texture = "custom_frames_flames"
+
+mod:hook(UIWidgets, "create_portrait_frame", function(func, ...)
+  local argos = {...}
+  local ret = func(...)
+  if argos[2] == "frame_holly_04" then
+    mod:echo("@@@@@@@@@@@@@@@@@@@@@@")
+    mod:dump(argos, "ARGS", 3)
+    mod:dump(ret, "RETURN", 3)
+    mod:echo("@@@@@@@@@@@@@@@@@@@@@@")
+  end
+  return ret
+end)
--- custom-frames/scripts/mods/custom-frames/custom-frames_data.lua
+++ custom-frames/scripts/mods/custom-frames/custom-frames_data.lua
@@ -1,24 +1,21 @@
 local mod = get_mod("custom-frames")
 
-local function build_custom_gui_textures(textures)
-  local atlases = {}
-  local chat_manager_injections = { "chat_manager" }
-  for i, tex in ipairs(textures) do
-    local path_material = "materials/mods/custom-frames/" .. tex
-    local path_atlas = path_material .. "_atlas"
-    atlases[i] = { path_atlas, tex }
-    chat_manager_injections[i + 1] = path_material
-  end
-
-  return {
-    atlases = atlases,
-    ui_renderer_injections = { chat_manager_injections },
-  }
-end
-
 return {
   name = "custom-frames",
   description = mod:localize("mod_description"),
   is_togglable = true,
-  custom_gui_textures = build_custom_gui_textures({ "frames" })
+  custom_gui_textures = {
+    atlases = {
+      {
+        "materials/mods/custom-frames/frames_atlas",
+        "materials/mods/custom-frames/frames",
+      },
+    },
+    ui_renderer_injections = {
+      {
+        "ingame_ui",
+        "materials/mods/custom-frames/frames",
+      },
+    }
+  }
 }
