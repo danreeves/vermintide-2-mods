1570ecbdd7eabd4cf580deff511c7378a5d0297d
Dan Reeves <hey@danreev.es>
2020-03-16 23:54:34 +0000

    [enable-imgui] make item crafting kinda work

--- enable-imgui/scripts/mods/enable-imgui/enable-imgui.lua
+++ enable-imgui/scripts/mods/enable-imgui/enable-imgui.lua
@@ -1,4 +1,5 @@
 local mod = get_mod("enable-imgui")
+local moreitemslibrary = get_mod("MoreItemsLibrary")
 
 -- Copied from source because I couldn't figure out how to require them
 -- or load the correct resource
@@ -15,3 +16,66 @@ end)
 function mod.on_enabled()
   mod.imgui_manager = ImguiManager:new()
 end
+
+mod:hook(ImguiCraftItem, 'give_item', function(func, self, item_key, power_level, skin_name, rarity, properties, traits)
+  mod:echo(item_key .. rarity)
+  local traits_list = {}
+  for trait,_ in pairs(traits) do
+    table.insert(traits_list, trait)
+  end
+  local entry = table.clone(ItemMasterList[item_key])
+  local backend_id = string.format("imgui-craft-item %d", math.random(10000))
+  entry.mod_data = {
+    backend_id = backend_id,
+    ItemInstanceId = backend_id,
+    CustomData = {
+      traits = cjson.encode(traits_list),
+      power_level = string.format("%d", power_level),
+      properties = cjson.encode(properties),
+      rarity = rarity,
+    },
+    traits = traits_list,
+    power_level = power_level,
+    properties = properties,
+    rarity = rarity,
+  }
+
+  if skin_name then
+    entry.mod_data.CustomData.skin = skin_name
+    entry.mod_data.skin = skin_name
+    entry.mod_data.inventory_icon = WeaponSkins.skins[skin_name].inventory_icon
+  end
+
+  moreitemslibrary:add_mod_items_to_local_backend({entry}, "enable-imgui")
+
+  local backend_items = Managers.backend:get_interface("items")
+  local new_item = backend_items:get_item_from_id(backend_id)
+
+  if rarity then
+    new_item.rarity = rarity
+    new_item.data.rarity = rarity
+    new_item.CustomData.rarity = rarity
+  end
+
+  ItemHelper.mark_backend_id_as_new(backend_id)
+
+  if mod.inventory_ui then
+    backend_items:_refresh()
+    local inv_item_grid = mod.inventory_ui._item_grid
+    local index = inv_item_grid._selected_page_index
+    local settings = inv_item_grid._category_settings[index]
+    local item_filter = settings.item_filter
+    inv_item_grid:change_item_filter(item_filter, false)
+    inv_item_grid:repopulate_current_inventory_page()
+  end
+end)
+
+mod.inventory_ui = nil
+
+mod:hook_safe(HeroWindowLoadoutInventory, "on_enter", function(self)
+  mod.inventory_ui = self
+end)
+
+mod:hook_safe(HeroWindowLoadoutInventory, "on_exit", function(self)
+  mod.inventory_ui = nil
+end)
