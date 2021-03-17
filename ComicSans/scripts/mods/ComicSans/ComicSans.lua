59ebbd2b5f486fb84bc8db0797fe0f19a851f990
Dan Reeves <hey@danreev.es>
2021-03-17 01:48:52 +0000

    lol add the skaven font too

--- ComicSans/scripts/mods/ComicSans/ComicSans.lua
+++ ComicSans/scripts/mods/ComicSans/ComicSans.lua
@@ -2,5 +2,7 @@
 local mod = get_mod("ComicSans")
 
 mod:hook(UIRenderer, "draw_text", function(func, self, text, font_material, font_size, font_name, position, color, retained_id, color_override)
-  return func(self, text, "fonts/mods/ComicSans/ComicSans", font_size, font_name, position, color, retained_id, color_override)
+  local font = mod:get("font")
+  local material = "fonts/mods/ComicSans/" .. font
+  return func(self, text, material, font_size, font_name, position, color, retained_id, color_override)
 end)
--- ComicSans/scripts/mods/ComicSans/ComicSans_localization.lua
+++ ComicSans/scripts/mods/ComicSans/ComicSans_localization.lua
@@ -2,4 +2,13 @@ return {
 	mod_description = {
 		en = "Replace all the fonts in the game with Comic Sans for optimal legibility",
 	},
+	ComicSans = {
+	  en = "Comic Sans",
+	},
+	Queekish = {
+	  en = "Queekish"
+	},
+	font = {
+	  en = "Font"
+	},
 }
