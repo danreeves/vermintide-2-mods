752afd1469f959ab67a8821cf549850c65f20a7d
Dan Reeves <hey@danreev.es>
2021-05-10 04:32:48 +0100

    only replace local user for now

--- custom-frames/scripts/mods/custom-frames/custom-frames.lua
+++ custom-frames/scripts/mods/custom-frames/custom-frames.lua
@@ -1,7 +1,12 @@
--- luacheck: globals get_mod callback Managers Material Gui UnitFrameUI UIUtils
+-- luacheck: globals get_mod callback Managers Material Gui UIUtils
+-- luacheck: globals UnitFramesHandler
 local mod = get_mod("custom-frames")
 local vmf = get_mod("VMF")
 local ui_renderers = vmf:persistent_table("_ui_renderers")
+local requested_textures = mod:persistent_table("requested_textures")
+local texture_resources = mod:persistent_table("texture_resources")
+local loading_id = 0
+local MAT_NAME = "template"
 
 mod:command("set_frame", "Set your custom frame URL", function(url)
   -- Fix for double paste
@@ -13,17 +18,10 @@ mod:command("set_frame", "Set your custom frame URL", function(url)
   end
 
   mod:set("frame", url)
-  mod.load_frame()
+  mod.load_frame(url)
 end)
 
-local MAT_NAME = "template"
-
-local requested_textures = mod:persistent_table("requested_textures")
-local texture_resources = mod:persistent_table("texture_resources")
-local loading_id = 0
-
-function mod.load_frame()
-  local texture_url = mod:get("frame")
+function mod.load_frame(texture_url)
 
   if not texture_url then
     return
@@ -75,10 +73,14 @@ function mod.on_all_mods_loaded()
   mod.set_diffuse_maps()
 end
 
-mod:hook_safe(UnitFrameUI, "set_portrait_frame", function(self)
-  mod.load_frame()
-
-  self._widgets.portrait_static.content.texture_1 = MAT_NAME
-  self._widgets.portrait_static.style.texture_1.size = {164, 186}
-  self._widgets.portrait_static.style.texture_1.offset = { -83, -77, 10}
+mod:hook_safe(UnitFramesHandler, "_sync_player_stats", function (self, unit_frame)
+	local player_data = unit_frame.player_data
+	local player = player_data.player
+    if player == Managers.player:local_player() then
+      local texture_url = mod:get("frame")
+      mod.load_frame(texture_url)
+      unit_frame.widget._widgets.portrait_static.content.texture_1 = MAT_NAME
+      unit_frame.widget._widgets.portrait_static.style.texture_1.size = {164, 186}
+      unit_frame.widget._widgets.portrait_static.style.texture_1.offset = { -83, -77, 10}
+    end
 end)
