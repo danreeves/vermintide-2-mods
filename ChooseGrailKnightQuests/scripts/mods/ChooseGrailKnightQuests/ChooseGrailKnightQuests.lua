b6d3c1229a97324e1ee16f57cdfe17693469fe16
Dan Reeves <hey@danreev.es>
2021-05-31 02:31:43 +0100

    fix choose gk quests randomisation

--- ChooseGrailKnightQuests/scripts/mods/ChooseGrailKnightQuests/ChooseGrailKnightQuests.lua
+++ ChooseGrailKnightQuests/scripts/mods/ChooseGrailKnightQuests/ChooseGrailKnightQuests.lua
@@ -7,16 +7,20 @@ mod:hook(PassiveAbilityQuestingKnight, "_generate_quest_pool", function(func, se
   local selected = {
     mod:get("quest1"),
     mod:get("quest2"),
-    mod:get("quest3"),
     "deus_" .. mod:get("quest1"),
     "deus_" .. mod:get("quest2"),
-    "deus_" .. mod:get("quest3"),
-    "markus_questing_knight_passive_strength_potion"
   }
+
+  if self._talent_extension:has_talent("markus_questing_knight_passive_additional_quest") then
+    table.insert(selected, mod:get("quest3"))
+    table.insert(selected, "deus_" .. mod:get("quest3"))
+  end
+
   for _, quest in pairs(quests) do
     if table.contains(selected, quest.reward) then
       table.insert(chosen_quests, quest)
     end
   end
+
   return chosen_quests
 end)
