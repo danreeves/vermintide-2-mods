79cfde80f34567643a0fa385dc314bfb0a6c9865
Dan Reeves <hey@danreev.es>
2023-01-08 00:26:18 +0000

    update more players

--- MorePlayers2/scripts/mods/MorePlayers2/MorePlayers2.lua
+++ MorePlayers2/scripts/mods/MorePlayers2/MorePlayers2.lua
@@ -5,7 +5,7 @@ function mod.on_all_mods_loaded()
 	mod.mmo_names = get_mod("MMONames2")
 end
 
-mod.VERSION = "0.37"
+mod.VERSION = "0.38"
 mod.MOD_NAME = "[BETA] BTMP"
 mod.MAX_PLAYERS = mod:get("max_players") or 32
 mod.ID = "2113204803" -- Steam Workshop ID
--- MorePlayers2/scripts/mods/MorePlayers2/src/bots.lua
+++ MorePlayers2/scripts/mods/MorePlayers2/src/bots.lua
@@ -3,10 +3,38 @@ local mod = get_mod("MorePlayers2")
 
 script_data.cap_num_bots = mod:get("num_bots")
 
-mod:hook(GameModeAdventure, "_get_first_available_bot_profile", function ()
-  return 1, 4
-end)
+local profiles = {
+	{ 1, 1 },
+	{ 1, 2 },
+	{ 1, 3 },
+	{ 1, 4 },
 
-mod:hook(GameModeDeus, "_get_first_available_bot_profile", function ()
-  return 1, 4
-end)
+	{ 2, 1 },
+	{ 2, 2 },
+	{ 2, 3 },
+	-- { 2, 4 },  -- sienna
+
+	{ 3, 1 },
+	{ 3, 2 },
+	{ 3, 3 },
+	{ 3, 4 },
+
+	{ 4, 1 },
+	{ 4, 2 },
+	{ 4, 3 },
+	{ 4, 4 },
+
+	{ 5, 1 },
+	{ 5, 2 },
+	{ 5, 3 },
+	{ 5, 4 },
+}
+
+local function random_bot()
+	local profile = table.random(profiles)
+	return profile[1], profile[2]
+end
+
+mod:hook(GameModeAdventure, "_get_first_available_bot_profile", random_bot)
+
+mod:hook(GameModeDeus, "_get_first_available_bot_profile", random_bot)
