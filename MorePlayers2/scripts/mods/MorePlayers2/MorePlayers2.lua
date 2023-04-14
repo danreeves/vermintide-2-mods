7fd70c0399414a6ea1621d2338f234300993ca4d
Dan Reeves <hey@danreev.es>
2023-04-14 12:03:25 +0100

    maybe fix beastmen banner crash

--- MorePlayers2/scripts/mods/MorePlayers2/MorePlayers2.lua
+++ MorePlayers2/scripts/mods/MorePlayers2/MorePlayers2.lua
@@ -7,7 +7,7 @@ function mod.on_all_mods_loaded()
 	mod.mmo_names = get_mod("MMONames2")
 end
 
-mod.VERSION = "0.42"
+mod.VERSION = "0.43"
 mod.MOD_NAME = "BTMP"
 mod.MAX_PLAYERS = mod:get("max_players") or 32
 mod.ID = "2113204803" -- Steam Workshop ID
--- MorePlayers2/scripts/mods/MorePlayers2/src/fix/beastmen_standard.lua
+++ MorePlayers2/scripts/mods/MorePlayers2/src/fix/beastmen_standard.lua
@@ -1,12 +1,13 @@
 -- luacheck: globals get_mod BeastmenStandardExtension
 local mod = get_mod("MorePlayers2")
 
-mod:hook(BeastmenStandardExtension, "init", function(func, self, ...)
-	func(self, ...)
+mod:hook_safe(BeastmenStandardExtension, "init", function(self)
 	if self.is_server then
-		local astar_data = self.player_astar_data[1]
-		for i = 1, mod.MAX_PLAYERS, 1 do
-			self.player_astar_data[i] = astar_data
+		local t = Managers.time:time("game")
+		for i = 5, mod.MAX_PLAYERS, 1 do
+			self.player_astar_data[i] = {
+				next_astar_check_t = t + self.astar_check_frequency,
+			}
 		end
 	end
 end)
